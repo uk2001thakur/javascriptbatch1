81af27cd8caea9a7b78356398811f87aeba81c40
UMESH KUMAR SINGH <63594650+uk2001thakur@users.noreply.github.com>
2022-01-29 18:05:46 +0530

    Add files via upload

---
+++ to-check-numbers.js
@@ -0,0 +1,13 @@
+const num = parseInt(prompt("Enter a number: "));
+
+if (num > 0) {
+    console.log("The number is positive");
+}
+
+else if (num == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative");
+}
